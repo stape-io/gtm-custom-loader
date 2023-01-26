6e8557df4a34c3b7a65e420e9ac80c389e003a2b
Kostiantyn Horozhanov <k.horozhanov@gmail.com>
2023-01-26 22:06:40 +0200

    getIsCookieKeeperEnabled refactoring

--- src/lib/getIsCookieKeeperEnabled.ts
+++ src/lib/getIsCookieKeeperEnabled.ts
@@ -1,17 +1,5 @@
 export default function getIsCookieKeeperEnabled(): boolean {
-  try {
-    const ua = navigator.userAgent.toLowerCase();
-    const isSafari = ua.indexOf('safari') !== -1 && ua.indexOf('chrome') === -1;
-    const version =
-      ua.indexOf('iphone') !== -1
-        ? ua
-            .replace(/.*\sos\s(\d+_\d+)\s.*/, '$1')
-            .split('_')
-            .join('.')
-        : ua.replace(/.*version\/(.*)\s.*/, '$1');
-    return isSafari && parseFloat(version) >= 16.4;
-  } catch (e) {
-    console.error(e);
-    return false;
-  }
+  const ua = navigator.userAgent;
+  const uaMatch = /Version\/([0-9\._]+)(.*Mobile)?.*Safari.*/.exec(ua);
+  return !!uaMatch && parseFloat(uaMatch[1]) > 16.4;
 }
